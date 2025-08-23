f28170f33c50f30e3ccee887645b15ca700d7b09
Danylo <beldan132@gmail.com>
2025-08-23 19:57:22 +0300

    SecurityConfig.java modified

--- src/main/java/technikal/task/fishmarket/security/SecurityConfig.java
+++ src/main/java/technikal/task/fishmarket/security/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,19 +18,24 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
+        UserDetails admin = User.builder()
                 .username("admin")
-                .password("admin")
+                .password(passwordEncoder().encode("admin"))
                 .roles("ADMIN")
                 .build();
 
-        UserDetails user = User.withDefaultPasswordEncoder()
+        UserDetails user = User.builder()
                 .username("user")
-                .password("user")
+                .password(passwordEncoder().encode("user"))
                 .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -37,15 +44,21 @@ public class SecurityConfig {
                 .authorizeHttpRequests((requests) -> requests
                             .requestMatchers("/fish/create", "/fish/delete/**").hasRole("ADMIN")
                             .requestMatchers("/fish").hasAnyRole("ADMIN", "USER")
+                            .requestMatchers("/login", "/").permitAll()
                             .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
                             .loginPage("/login")
                             .permitAll()
+                            .defaultSuccessUrl("/fish", true)
                 )
                 .logout((logout) -> logout
                         .permitAll()
-                );
+                        .logoutSuccessUrl("/login")
+                )
+               .exceptionHandling((exception) -> exception
+                       .accessDeniedPage("/login")
+               );
 
             return http.build();
     }
