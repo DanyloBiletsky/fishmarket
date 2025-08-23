772e899732afbccd790215f3d4006c72950e866a
Danylo <beldan132@gmail.com>
2025-08-23 22:23:59 +0300

    SecurityConfig.java modified Trying to resolve bug

--- src/main/java/technikal/task/fishmarket/app/Application.java
+++ src/main/java/technikal/task/fishmarket/app/Application.java
@@ -1,13 +1,13 @@
-package technikal.task.fishmarket;
+package technikal.task.fishmarket.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FishmarketApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FishmarketApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/technikal/task/fishmarket/security/SecurityConfig.java
+++ src/main/java/technikal/task/fishmarket/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package technikal.task.fishmarket.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -39,6 +41,11 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
        http
                 .authorizeHttpRequests((requests) -> requests
@@ -51,6 +58,7 @@ public class SecurityConfig {
                             .loginPage("/login")
                             .permitAll()
                             .defaultSuccessUrl("/fish", true)
+                            .failureUrl("/login?error")
                 )
                 .logout((logout) -> logout
                         .permitAll()
